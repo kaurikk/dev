4933582e5ec3a66f93a7d554cb703d88ff7adab7
Kauri Kont-Kontson <kaurikk@gmail.com>
2014-11-15 17:38:56 +0200

    Factory test

---
+++ PHP/test/FactoryTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kauri
+ * Date: 15/11/14
+ * Time: 17:37
+ */
+
+class FactoryTest extends PHPUnit_Framework_TestCase
+{
+  //
+}
+ 
--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 
-require_once 'PHP/__autoloader.php';
+require_once 'PHP/autoloader.php';
 
 use PHP\Factory;
 
 // Build a car
 $car = Factory::build(Factory::VEHICLE_CAR, 'BMW', 'M6', 'silver');
-$bike = Factory::build(Factory::VEHICLE_MOTORCYCLE, 'BMW', 'RS1200', 'black');
+
 // Accelerate to 50km/h
 $car->accelerate(50);
-$car->brake(40);
-
 echo sprintf('Current speed: %s km/h' . "\n", $car->getCurrentSpeed());
 
+// Break to 40
+$car->brake(40);
+echo sprintf('Current speed: %s km/h' . "\n", $car->getCurrentSpeed());
 
-
-
-
+// Break to 0
+$car->brake(30);
+echo sprintf('Current speed: %s km/h' . "\n", $car->getCurrentSpeed());
 
 
 echo sprintf('No of vehicles built: %s' . "\n", Factory::getNoOfVehiclesBuilt());
